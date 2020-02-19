ddcbf99cbdbf7f200a7284d5c65fa01765bf1ebf
김광준 <kwangjun3952@gmail.com>
2020-02-19 20:30:54 +0900

    Leacture-4-3

---
+++ Leacture-4-3/asyncProgrammingUsedDispatchQueue/asyncProgrammingUsedDispatchQueue/MainTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MainTableViewCell.swift
+//  asyncProgrammingUsedDispatchQueue
+//
+//  Created by 김광준 on 2020/02/19.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    static let mainTableViewCellId: String = "MainTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Leacture-4-3/asyncProgrammingUsedDispatchQueue/asyncProgrammingUsedDispatchQueue/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  asyncProgrammingUsedDispatchQueue
+//
+//  Created by 김광준 on 2020/02/19.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var mainTableView: UITableView = {
+        var mainTableView: UITableView = UITableView()
+        mainTableView.register(MainTableViewCell.self, forCellReuseIdentifier: MainTableViewCell.mainTableViewCellId)
+        return mainTableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
